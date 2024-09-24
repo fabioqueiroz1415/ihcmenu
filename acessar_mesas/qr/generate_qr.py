b2c90a07cb657e824825ba65d4bb53b84822e724
fabioqueiroz1415 <fabioaureliodedeus@gmail.com>
2024-09-24 12:28:38 -0300

    update database.py

--- acessar_mesas/qr/generate_qr.py
+++ acessar_mesas/qr/generate_qr.py
@@ -1,5 +1,4 @@
 import qrcode
-import socket
 import sys
 import os
 
@@ -19,7 +18,5 @@ def generate_qr(s = "/"):
     return img
 
 def get_server_rote():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    site_url = f"http://{local_ip}:5000"
+    site_url = f"https://ihcmenu.pythonanywhere.com"
     return site_url
--- acessar_mesas/redirecionar/main.py
+++ acessar_mesas/redirecionar/main.py
@@ -1,7 +1,5 @@
-import socket
+
 
 def get_server_rote():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    site_url = f"http://{local_ip}:5000"
+    site_url = f"https://ihcmenu.pythonanywhere.com"
     return site_url
